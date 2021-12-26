4147bd05418e863d798d2a58cd98a058d2ac78df
takato0024 <takato1224ons@gmail.com>
2021-12-26 23:51:49 +0900

    linebot

--- app/views.py
+++ app/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse,HttpResponseBadRequest,HttpResponseServerError
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+line_bot_api = LineBotApi(settings.CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(settings.CHANNEL_SECRET)
 
 class CallbackView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('OK')
+
+    def post(self, request, *args, **kwargs):
+        #リクエストヘッダから署名検証のための値を取得
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        #リクエストボディを取得
+        body = request.body.decode('utf-8')
+        
+        try:
+            handler.handle(body,signature)
+        except InvalidSignatureError:   #署名検証失敗
+            return HttpResponseBadRequest()
+        except LineBotApiError:     #APIエラー
+            return HttpResponseServerError()
+        
+        return HttpResponse('OK')
+    
+    #外部からのアクセスを可能にする
+    #(csrf_tokenを渡していないpostメソッドは403エラーになるため)
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(CallbackView, self).dispatch(*args, **kwargs)
+    
+    #staticmethodはインスタンス化せずに呼び出せる関数のこと
+    #handlerのaddメソッドで、リクエストのイベント毎に実行する関数を記述
+    @staticmethod
+    @handler.add(MessageEvent, message=TextMessage)
+    def message_event(event):
+        #オウム返し
+        reply = event.message.text
+        
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply)
+        )
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jxkv_d#)ownq10-dc_&jt&a3hcf!bwo4ff88e8z+3b4z-yett5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"] #本来はドメインの方がよい
 
 
 # Application definition
@@ -128,4 +128,7 @@ except ImportError:
 
 if not DEBUG:
     import django_heroku
+    import os
+    CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+    CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
     django_heroku.settings(locals())
